f930e07970246425a8d87d7f124f373ea8c647d0
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-09 04:09:40 -0300

    Classe principal, que será gerada posteriormente pelo selenium_proc_control, responsável pela solicitação do teste funcional

---
+++ main.rb
@@ -0,0 +1,21 @@
+#require "selenium-webdriver"
+require_relative 'event/event'
+
+#driver = Selenium::WebDriver.for :chrome
+#driver.navigate.to "http://189.3.216.130:8080/ventaboletosadm"
+
+#element = driver.find_element(name: 'j_username')
+#element.send_keys ""
+
+#element = driver.find_element(name: 'j_password')
+#element.send_keys ""
+
+#element.submit
+
+#puts driver.title
+
+#driver.quit
+
+event = Event::Keyboard::INSTANCE
+event.findBy('teste', '123')
+
