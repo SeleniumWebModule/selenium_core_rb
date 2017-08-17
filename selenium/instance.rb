4ec4d0b06e876336277331d95596c71e4d071e58
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-17 08:42:50 -0300

    Configuração de instancia para maximizar a tela durante o teste. Remoção de configuração que permite ao browser ficar aberto após encerramento do teste.

--- selenium/instance.rb
+++ selenium/instance.rb
@@ -2,14 +2,16 @@ require "selenium-webdriver"
 
 class SeleniumInstance
 	def initialize(url, port, context)
-		options =  Selenium::WebDriver::Chrome::Options.new
-		options.add_option(:detach, true)
-		@driver = Selenium::WebDriver.for(:chrome, options: options)
+		#options =  Selenium::WebDriver::Chrome::Options.new
+		#options.add_option(:detach, true)
+		#@driver = Selenium::WebDriver.for(:chrome, options: options)
+		@driver = Selenium::WebDriver.for(:chrome)
 		openSession(url, port, context)
 	end
 
 	def openSession(url, port, context)
 		@driver.navigate.to "http://#{url}:#{port}/#{context}"	
+		@driver.manage.window.maximize
 	end
 
 	attr_accessor :driver
