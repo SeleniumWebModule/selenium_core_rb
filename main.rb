6487f2138ecfeddb606fbfb62ad60d6ebfce39a1
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-17 08:37:03 -0300

    Sequencia na criação de caso de teste para demonstração

--- main.rb
+++ main.rb
@@ -35,32 +35,37 @@ require_relative 'rule/rule'
 
 #puts Event::Keyboard::FIND.by('id')
 
-seleniumSystem = Model::System.new('189.3.216.130', '8080', 'ventaboletosadm').instance
-screenLogin = Model::Screen.new('Login').instance
-
-userFindBy = Model::FindBy.new(IdentifyBy::NAME, 'j_username').instance
-user = Model::Component.new('user').instance
-user.registerAttribute(Model::Attribute.new('findBy', userFindBy))
-user.registerAttribute(Model::Attribute.new('value', 'gleimar'))
-user.registerEvent(Event::Keypress::INSTANCE)
-screenLogin.registerComponent(user)
-
-passUserFindBy = Model::FindBy.new(IdentifyBy::NAME, 'j_password').instance
-passUser = Model::Component.new('password').instance
-passUser.registerAttribute(Model::Attribute.new('findBy', passUserFindBy))
-passUser.registerAttribute(Model::Attribute.new('value', 'gleimar'))
-passUser.registerEvent(Event::Keypress::INSTANCE)
-screenLogin.registerComponent(passUser)
-
-btnAcessoFindBy = Model::FindBy.new(IdentifyBy::TAG_NAME, 'input').instance
-btnAcesso = Model::Component.new('acesso').instance
-btnAcesso.registerAttribute(Model::Attribute.new('findBy', btnAcessoFindBy))
-btnAcesso.registerAttribute(Model::Attribute.new('value', 'Acesso'))
-btnAcesso.registerEvent(Event::MouseClick::INSTANCE)
-screenLogin.registerComponent(btnAcesso)
-
-seleniumSystem.registerScreen(screenLogin)
-seleniumSystem.validate
-
 #btnAcesso = Model::Component.new('Acesso')
 
+def login
+	
+	screenLogin = Model::Screen.new('Login').instance
+
+	userFindBy = Model::FindBy.new(IdentifyBy::NAME, 'j_username').instance
+	user = Model::Component.new('user').instance
+	user.registerAttribute(Model::Attribute.new('findBy', userFindBy))
+	user.registerAttribute(Model::Attribute.new('value', 'gleimar'))
+	user.registerEvent(Event::Keypress::INSTANCE)
+	screenLogin.registerComponent(user)
+
+	passUserFindBy = Model::FindBy.new(IdentifyBy::NAME, 'j_password').instance
+	passUser = Model::Component.new('password').instance
+	passUser.registerAttribute(Model::Attribute.new('findBy', passUserFindBy))
+	passUser.registerAttribute(Model::Attribute.new('value', 'gleimar'))
+	passUser.registerEvent(Event::Keypress::INSTANCE)
+	screenLogin.registerComponent(passUser)
+
+	btnAcessoFindBy = Model::FindBy.new(IdentifyBy::TAG_NAME, 'input').instance
+	btnAcesso = Model::Component.new('acesso').instance
+	btnAcesso.registerAttribute(Model::Attribute.new('findBy', btnAcessoFindBy))
+	btnAcesso.registerAttribute(Model::Attribute.new('value', 'Acesso'))
+	btnAcesso.registerEvent(Event::MouseClick::INSTANCE)
+	screenLogin.registerComponent(btnAcesso)
+
+	return screenLogin
+end
+
+seleniumSystem = Model::System.new('189.3.216.130', '8080', 'ventaboletosadm').instance
+seleniumSystem.registerScreen(login)
+seleniumSystem.validate
+seleniumSystem.mapMenu(Model::FindBy.new(IdentifyBy::CLASS_NAME, 'z-menubar-hor').instance);
