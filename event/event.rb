2d274b7aab2c704770bf121cee002e9c5b11d39d
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-17 01:38:00 -0300

    Criação de fluxo para lançamento do evento OnClick

--- event/event.rb
+++ event/event.rb
@@ -1,8 +1,13 @@
 require_relative 'keyboard/keypressevent'
+require_relative 'mouse/clickevent'
 require_relative '../model/findby'
 
 module Event 
   class Keypress 
   	INSTANCE = KeypressEvent.new
   end  
+
+  class MouseClick
+  	INSTANCE = OnClickEvent.new
+  end
 end
