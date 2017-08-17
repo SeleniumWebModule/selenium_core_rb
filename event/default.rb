082688e327e84b6854f03ddf4a5ace722ca8eb5d
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-17 01:36:47 -0300

    Criação de método genérico para recuperação de attributo

--- event/default.rb
+++ event/default.rb
@@ -1,5 +1,9 @@
 require_relative '../model/findby'
 
 class Default 
-	
+	def findAttribute(attributes, key)
+		for attribute in attributes
+			 return attribute if attribute.id == key
+		end
+	end
 end
