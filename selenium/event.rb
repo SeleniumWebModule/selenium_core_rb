88a7c3bb2a317f3b11e3c55efe4918f28a922042
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-17 01:42:18 -0300

    Recuperação de elemento via variável global. Recuperação de elementos e filtro pelo id e pelo value

--- selenium/event.rb
+++ selenium/event.rb
@@ -2,7 +2,18 @@ require "selenium-webdriver"
 require_relative "instance"
  
 class SeleniumEvent
-	def findElement(attribute)
-		return Selenium::EVENT::INSTANCE.find_element("#{attribute.findby.identifyBy}": 'j_username')
+	def findElement(findBy)
+		return $seleniumInstance.driver.find_element("#{findBy.identifyBy}": "#{findBy.value}")
+	end
+
+	def findElements(findBy, id, value)
+		elements = $seleniumInstance.driver.find_elements("#{findBy.identifyBy}": "#{findBy.value}")
+		
+		for element in elements
+			if element.attribute("#{id}") == value
+				return element
+				break
+			end
+		end
 	end
 end
